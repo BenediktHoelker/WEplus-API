d68bac82e6f674fe43c6a08e86a6db33958057a9
BenediktHoelker <benedikt.hoelker@gmail.com>
2016-11-29 23:20:45 +0100

    No select for production

--- app.js
+++ app.js
@@ -14,9 +14,11 @@ if(process.env.REDISTOGO_URL){
 }
 else{
   var client = redis.createClient();
+
+  client.select((process.env.NODE_ENV || 'development').length);
 }
 
-client.select((process.env.NODE_ENV || 'development').length);
+
 
 app.get('/', function(request, response)
 {
