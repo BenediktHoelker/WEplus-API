e6389c19ae08dabbe91638c680a9e99dbd443c83
BenediktHoelker <benedikt.hoelker@gmail.com>
2016-11-30 08:05:22 +0100

    Add validations for new city

--- test.js
+++ test.js
@@ -62,7 +62,15 @@ describe('Creating new cities', function(){
       .send('name=Springfield&description=where+the+simpsons+live')
       .expect(/springfield/i, done);
   });
+  it('Validates city name and description', function(done){
+
+    request(app)
+      .post('/cities')
+      .send('name=&description=')
+      .expect(400,done);
+  });
 });
+
 
 describe('Deleting cities', function(){
   before(function(){
